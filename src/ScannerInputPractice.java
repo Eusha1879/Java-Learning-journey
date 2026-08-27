020f64193b3f5bc07e56eb4a74837ee46d7dfe3b
Eusha Ibne Nur Sami <saminfs9@gmail.com>
2026-08-27 08:37:08 +0100

    Practice with Scanner keyboard & User Inputs - D1

---
+++ src/ScannerInputPractice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ScannerInputPractice {
+
+    public static  void main (String [] args){
+
+        System.out.println("============= Dear Eusha Wellcome to the CRO Interview ====== ");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Tell Me about yourself Eusha : ");
+        String answer1 = scanner.nextLine();
+        System.out.println("Tell me a situation you've handled in ur previous site :  ");
+        String answer2 = scanner.nextLine();
+        System.out.println("Thank you for your recent application for the Control Room Operator" +
+                " role. Unfortunately, on this occasion," +
+                " we will not be moving forward with your application."
+               );
+
+
+        scanner.close();
+    }
+}
+
+
+// so basically as a beginner i just work here with Scanner class and user Input and basic coding practic.
